623194517d956eddd07d651872ce8f81fb826ea9
PreacherKeith <dokeith2005@gmail.com>
2025-04-08 07:46:01 -0400

    Delete bootstrap.php

--- bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("PROJECT_ROOT_PATH", __DIR__ . "/../");
-// include main configuration file 
-require_once PROJECT_ROOT_PATH . "/inc.php";
-// include the base controller file 
-require_once PROJECT_ROOT_PATH . "/basecontroller.php";
-// include the use model file 
-require_once PROJECT_ROOT_PATH . "/usermodel.php";
-?>
